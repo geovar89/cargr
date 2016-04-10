ce9fdfd80496d3c214f60678fbe66c8b19ba099f
geovar89 <geovar89@gmail.com>
2016-04-10 14:05:23 +0300

    use real urls

--- public_html/external.php
+++ public_html/external.php
@@ -2,12 +2,12 @@
     $pg   = $_POST["pg"];
     
     if ($pg == "0") {
-        //$var1 = 'http://www.car.gr/classifieds/cars/?h&sort=cr&rg=3&offer_type=sale&created=1&registration=1995-2016&st=p&pg=' . $pg;
-        $var1 = 'http://localhost:8080/cargr/public_html/test2.html';
+        $var1 = 'http://www.car.gr/classifieds/cars/?h&sort=cr&rg=3&offer_type=sale&created=1&registration=1995-2016&st=p&pg=' . $pg;
+        //$var1 = 'http://localhost:8080/cargr/public_html/test2.html';
         $result = file_get_contents($var1);
     } else {
-        //$var1 = 'http://www.car.gr/classifieds/cars/?sort=cr&rg=3&offer_type=sale&created=1&registration=1995-2016&st=p&pg=' . $pg;
-        $var1 = 'http://localhost:8080/cargr/public_html/test.html';
+        $var1 = 'http://www.car.gr/classifieds/cars/?sort=cr&rg=3&offer_type=sale&created=1&registration=1995-2016&st=p&pg=' . $pg;
+        //$var1 = 'http://localhost:8080/cargr/public_html/test.html';
         $result = file_get_contents($var1);
     }
     echo $result;
